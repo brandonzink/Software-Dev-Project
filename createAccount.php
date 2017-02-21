8a8bc3ed6e57b755bbae36b18940d3f2c6426c3b
Brandon Zink <brandon.zink@colorado.edu>
2017-02-20 17:13:49 -0700

    Finished login page setup, created account creation page

---
+++ createAccount.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Account</title>
+
+	<style type="text/css">
+	/*Setting up styling for page*/
+		body{
+			background-color: gainsboro;
+		}
+		h3{
+			font-family: Arial;
+		}
+
+		p{
+			font-family: Arial;
+			font-size: 14px;
+		}
+
+		p1{
+			font-family: Arial;
+			color: grey;
+			font-size: 11px;
+		}
+
+		p2{
+			font-family: Arial;
+		}
+
+	</style>
+</head>
+<body>
+	<CENTER>
+	<br><br><br><br><br><br>
+	<h3>Let's make a new account for you here at FaceIt.</h2>
+	<p>Please come up with a username, password, and enter your email address.<p>
+
+	<!-- Headers and instructions -->
+
+	<form method="post" id="createAccount" name="createAccount" action="scriptsPHP/accountCreation.php">
+
+	<!-- Sends information to createAccount.php -->
+
+  		<p2>Username:</p2><br>
+  		<!-- Max 20 characters -->
+  		<p1>Please make it 20 characters or less.</p1><br>
+  		<input type="text" placeholder="username" name="username"><br> 
+  		<!-- Username field -->
+  		<br><p2>Password:</p2><br>
+  		<input type="password" placeholder="password" name="password">
+  		<!-- Password field -->
+  		<br><br><p2>Email:</p2><br>
+  		<input type="email" placeholder="me@example.net" name="email"><br>
+  		<!-- Email field -->
+  		<br><input type="Submit" class="createAccount" value="Create Account" onclick="return validateForm()" /></input> 
+  		<!-- Runs validateForm() to check if fields are empty -->
+	</form>
+	</CENTER>
+</body>
+</html>
+
+<script type="text/javascript">
+// Checks to see if fields are left empty, shows alert message if any are empty
+function validateForm()
+{
+	var a=document.forms["createAccount"]["username"].value;
+    var b=document.forms["createAccount"]["password"].value;
+    var c=document.forms["createAccount"]["email"].value;
+    if(a==null || a=="",b==null || b=="", c==null || c=="")
+    {
+    	alert("Please fill all reuired fields");
+    	return false;
+    }
+}
+</script>
+
--- index.php
+++ index.php
@@ -13,17 +13,42 @@ body{
 	
   <CENTER>
   <img src="http://i.imgur.com/NlLgS1e.png" id="sslogo"/>
-  <form id="LoginForm" action="submit" method="post">
+  <!-- Site logo -->
+  <form method="post" id="loginForm" name="loginForm" action="scriptsPHP/loginForm.php">
+  <!-- Sends info to loginForm.php -->
   		Username:<br>
   		<!-- Max 20 characters -->
   		<input type="text" name="username"><br> 
+  		<!-- Username entry field -->
   		Password:<br>
   		<input type="password" name="password">
+  		<!-- Password entry field -->
   		<br>
-  		<br><button type="button">Login</button>
+  		<br><input type="Submit" class="login" value="Login" onclick="return validateForm()" /></input> 
+  		<!-- Checks to see if fields are left empty -->
 	</form>
+	<br>
+	<a href="createAccount.php">
+		<button>Create Acount</button>
+	</a>
+	<!-- Sends to create account page -->
+
     </CENTER>
 
 </header> 
 </body>
 </html>
+
+<script type="text/javascript">
+// Checks to see if fields are blanks, alerts if they are
+function validateForm()
+{
+	var a=document.forms["loginForm"]["username"].value;
+    var b=document.forms["loginForm"]["password"].value;
+    if(a==null || a=="",b==null || b=="")
+    {
+    	alert("Please fill all reuired fields");
+    	return false;
+    }
+}
+</script>
