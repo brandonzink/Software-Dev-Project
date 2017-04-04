016d71a83adc461494ba4086eb914f4a91babf51
abfa4082 <abfa4082@colorado.edu>
2017-04-04 16:06:49 -0600

    profile page
    added input boxes

--- scriptsPHP/profilePage.php
+++ scriptsPHP/profilePage.php
@@ -9,26 +9,27 @@
 
 <?php
 include_once("databasehelper.php");
-include_once("loginForm.php");
-
-echo "Username<br>";
-echo "johndoe<br>";
-
-echo "First Name<br>";
-echo "John<br>";
-
-echo "Last Name<br>";
-echo "Doe<br>";
-
-echo "Profession<br>";
-echo "Accountant<br>";
-
-echo "Description<br>";
-//ADD AN INPUT BOX
-
-
+//include_once("loginForm.php");
 
 
 ?>
+
+<form action="profilePage2">
+  <fieldset>
+    <legend>Personal information:</legend>
+    First name:<br>
+    <input type="text" name="firstname" value=""><br><br>
+    Last name:<br>
+    <input type="text" name="lastname" value=""><br><br>
+    Username:<br>
+    <input type="text" name="username" value=""><br><br>
+    Profession:<br>
+    <input type="text" name="profession" value=""><br><br>
+    Description:<br>
+    <input type="text" name="Description" value=""><br><br> 
+
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
 
 </html>
