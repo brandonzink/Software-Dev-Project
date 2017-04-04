3785b127fbc98a6d79ae66c6171909671604ecb5
abfa4082 <abfa4082@colorado.edu>
2017-04-04 14:46:17 -0600

    Hard coded profile page.
    Not yet finished

---
+++ scriptsPHP/profilePage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<title>Profile Page</title>
+<head>
+<h1>Profile Page</h1>
+</head>
+
+<body></body>
+
+<?php
+include_once("databasehelper.php");
+include_once("loginForm.php");
+
+echo "Username<br>";
+echo "johndoe<br>";
+
+echo "First Name<br>";
+echo "John<br>";
+
+echo "Last Name<br>";
+echo "Doe<br>";
+
+echo "Profession<br>";
+echo "Accountant<br>";
+
+echo "Description<br>";
+//ADD AN INPUT BOX
+
+
+
+
+?>
+
+</html>
