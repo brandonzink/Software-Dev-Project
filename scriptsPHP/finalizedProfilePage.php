c079d4514dc4d9484b86cd7b2069d853df00ea6a
abfa4082 <abfa4082@colorado.edu>
2017-04-06 11:43:35 -0600

    More Profile page stuff
    I made it so that the user can write his info and once he submits it it
    takes him to a finalized profile page with the information he typed in.

---
+++ scriptsPHP/finalizedProfilePage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<title>Profile Page</title>
+
+<head>
+<h1>Profile Page</h1>
+</head>
+
+
+<?php
+
+
+//include_once("databasehelper.php");
+//include_once("profilePage.php");
+
+
+?>
+
+<body>
+
+First Name: <?php echo $_GET["firstname"]; ?><br><br>
+Last Name: <?php echo $_GET["lastname"]; ?><br><br>
+Username: <?php echo $_GET["username"]; ?><br><br>
+Profession: <?php echo $_GET["profession"]; ?><br><br>
+Description: <?php echo $_GET["description"]; ?><br><br>
+
+
+</body>
+
+
+<html>
--- scriptsPHP/profilePage.php
+++ scriptsPHP/profilePage.php
@@ -5,31 +5,35 @@
 <h1>Profile Page</h1>
 </head>
 
-<body></body>
+
 
 <?php
 include_once("databasehelper.php");
+//include_once("finalizedProfilePage.php");
 //include_once("loginForm.php");
 
 
+//value="Submit" onclick="return editProfilePage($mysqli, $firstname, $lastname, $username, $description)" /></input
 ?>
 
-<form action="profilePage2">
+<form action="finalizedProfilePage.php" method="get">
   <fieldset>
     <legend>Personal information:</legend>
     First name:<br>
-    <input type="text" name="firstname" value=""><br><br>
+    <input type="text" name="firstname"><br><br>
     Last name:<br>
-    <input type="text" name="lastname" value=""><br><br>
+    <input type="text" name="lastname"><br><br>
     Username:<br>
-    <input type="text" name="username" value=""><br><br>
+    <input type="text" name="username"><br><br>
     Profession:<br>
-    <input type="text" name="profession" value=""><br><br>
+    <input type="text" name="profession"><br><br>
     Description:<br>
-    <input type="text" name="Description" value=""><br><br> 
+    <input type="text" name="description"><br><br> 
 
-    <input type="submit" value="Submit">
+   <input type="submit">
   </fieldset>
 </form>
+
+
 
 </html>
